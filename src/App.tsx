7d0ed5b6ad19d4fb1f6d0406f77f1cf1fe80df1f
Leandro Martinez <leandro.martinez@thelucky.app>
2024-07-29 11:19:25 -0300

    add another option created in 40 minutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import "./App.css";
-import { useState, useEffect } from "react";
-import { PokemonItem } from "./components/PokemonItem";
-
 // Objective:
 // Create a React app that displays a list of pokemons along with their information
 
@@ -23,53 +19,87 @@ import { PokemonItem } from "./components/PokemonItem";
 // regardless of whatever input has been entered in the text field.
 // If disabled, display only the filtered pokemons
 
-const URL_POKEAPI =
+import { useState, useEffect } from "react";
+
+const POKEMONES_API =
   "https://gist.githubusercontent.com/nibble-4bits/6191e872f6ea39f3a2c181d3d29a300a/raw/9fbc467a8ab9d561bf3d16cb69a1352cb65597e6/pokedex.json";
 
+interface IBase {
+  HP: number;
+  Attack: number;
+  Defense: number;
+  "Sp. Attack": number;
+  "Sp. Defense": number;
+  Speed: number;
+}
+
+interface IPokemon {
+  name: string;
+  type: string[];
+  description: string;
+  base: IBase;
+  thumbnail: string;
+}
+
 export default function App() {
-  const [pokemones, setPokemones] = useState([]);
+  const [pokemones, setPokemones] = useState<IPokemon[]>([]);
   const [filter, setFilter] = useState("");
   const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
-    fetch(URL_POKEAPI)
-      .then((response) => response.json())
-      .then((data) => {
-        setPokemones(data);
-      })
-      .catch((error) => console.log("Error fetching data:", error));
+    fetch(POKEMONES_API)
+      .then((res) => res.json())
+      .then((body) => {
+        const pokemonesArray: IPokemon[] = [];
+        body.map((pokemon: any) => {
+          const pokemonObject = {
+            name: pokemon.name.english,
+            type: pokemon.type,
+            description: pokemon.description,
+            base: pokemon.base,
+            thumbnail: pokemon.image.thumbnail,
+          };
+          pokemonesArray.push(pokemonObject);
+        });
+        setPokemones(pokemonesArray);
+      });
   }, []);
 
   const filteredPokemones =
     filter && !showAll
       ? pokemones.filter(
-          (pokemon: any) =>
-            pokemon["name"]["english"].toLowerCase() === filter.toLowerCase()
+          (pokemon) => pokemon.name.toLowerCase() === filter.toLowerCase()
         )
       : pokemones;
 
   return (
-    <div className="App">
-      <label htmlFor="filter">Filter: </label>
+    <>
+      <h1>Pokemon List:</h1>
       <input
         type="text"
+        placeholder="Bulbasaur"
         name="filter"
-        placeholder="filter by name"
         onChange={(e) => setFilter(e.target.value)}
       />
-      Show all:{" "}
       <input
         type="checkbox"
         checked={showAll}
         onChange={() => setShowAll(!showAll)}
       />
+
       <ul>
-        {filteredPokemones.length
-          ? filteredPokemones.map((pokemon) => (
-              <PokemonItem pokemon={pokemon} />
-            ))
-          : "No se encontraron pokemones"}
+        {filteredPokemones.map((pokemon) => (
+          <li>
+            Name: {pokemon.name} <br />
+            Types: {pokemon.type.join(", ")} <br />
+            description: {pokemon.description} <br />
+            HP: {pokemon.base.HP} - Attack: {pokemon.base.Attack} - SP Attack:{" "}
+            {pokemon.base["Sp. Attack"]} - Sp Defense:{" "}
+            {pokemon.base["Sp. Defense"]} - Speed: {pokemon.base.Speed} <br />
+            Thumbnail: <img src={pokemon.thumbnail} alt={pokemon.name} />
+          </li>
+        ))}
       </ul>
-    </div>
+    </>
   );
 }
